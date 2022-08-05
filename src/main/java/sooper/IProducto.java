75751516fe53c6345dc394efb5cb26b8e291a4bd
Ospino <ospinotech@gmail.com>
2022-08-05 12:32:09 -0500

    Creacion de interfaces

---
+++ src/main/java/sooper/IProducto.java
@@ -0,0 +1,17 @@
+package sooper;
+
+public interface IProducto {
+
+    String getReferencia();
+
+    int getPeso();
+
+    int getVolumen();
+
+    String getCategoria();
+
+    boolean esCompatible(IProducto p);
+
+    boolean tengoEspacio(IContenedor contenedor);
+
+}
